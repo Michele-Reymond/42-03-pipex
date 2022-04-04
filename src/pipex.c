929a715366b61e9e3b72be55acb878da9e10db51
Michele Reymond <mreymond@c1r11s4.42lausanne.ch>
2022-04-04 17:09:51 +0200

    commencer a faire les erreurs

--- src/pipex.c
+++ src/pipex.c
@@ -6,24 +6,34 @@
 /*   By: mreymond <mreymond@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 14:56:23 by mreymond          #+#    #+#             */
-/*   Updated: 2022/03/30 15:38:26 by mreymond         ###   ########.fr       */
+/*   Updated: 2022/04/04 17:08:19 by mreymond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-// void	pipex(int fd1, int fd2)
-// {
-// 	int	end[2];
-
-// 	pipe(end);
-// }
-
-int	main(void)
+int	check_args(int argc, char **argv)
 {
-	int id = fork();
-	if (id != 0)
-		fork();
-	printf("Hello! from %d\n", id);
+	if (argc != 5)
+	{
+		printf("(Error) Too few or too much arguments\n");
+		exit(1);
+	}
+	// if(access("ACCESS.C",F_OK)==0) {
+	// 	printf("Le fichier «ACCESS.C» existe !\n");
+	// } else {
+	// 	printf("Le fichier «ACCESS.C» n'existe pas !\n");
+	// } 
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	pid_t	pid1;
+	pid_t	pid2;
+	int		fd[2];
+
+	check_args(argc, argv);
+	if (pipe(fd) == -1)
+		return (1);
 	return (0);
 }
